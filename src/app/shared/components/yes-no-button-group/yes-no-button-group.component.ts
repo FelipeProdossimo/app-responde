dab9e1a3200c626b1b3a3ca384725ac9233c7211
Felipe Prodóssimo <73621753+FelipeProdossimo@users.noreply.github.com>
2022-06-02 15:55:09 -0300

    Atualizando componentes e versões!

--- src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/shared/components/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,16 +1,27 @@
-import { Component, Input, OnInit, Output } from '@angular/core';
+import { Component, forwardRef, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { EventEmitter } from 'protractor';
 
 @Component({
   selector: 'app-yes-no-button-group',
   templateUrl: './yes-no-button-group.component.html',
-  styleUrls: ['./yes-no-button-group.component.scss']
+  styleUrls: ['./yes-no-button-group.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      multi: true,
+      useExisting: forwardRef() => YesNoButtonGroupComponent
+    }
+  ]
 })
-export class YesNoButtonGroupComponent implements OnInit {
+export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
 @Input() public value: string = null;
 @Input() public label = '';
-@Output() public valueChange = new EventEmitter();
+@Output() public valueChange = new EventEmitter<string>();
 public options = YesNoButtonGroupOptions;
+public onTouched() => {};
+
+}
 
   constructor() { }
 
